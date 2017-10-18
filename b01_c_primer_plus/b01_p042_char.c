aadc077f1e0f638ddd60c140e5a376c66f2460b8
zxwtry <zxwtry@qq.com>
2017-10-18 14:16:07 +0800

    Initial commit

---
+++ b01_c_primer_plus/b01_p042_char.c
@@ -0,0 +1,19 @@
+//
+// Created by Administrator on 2017/10/17.
+//
+
+#include <stdio.h>
+
+/*
+ *  ASCII: 0-127
+ *
+ */
+
+void b01_p042() {
+    char a = 97;
+    printf("%c\n", a);
+    
+    
+    int b = '\032';
+    printf("%d", b);
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    b01_p042();
+    printf("Hello, World!\n");
+    return 0;
+}

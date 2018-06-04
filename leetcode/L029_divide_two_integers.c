2c348bf59a67cb67f72ddbd8714a7f6266193167
zxwtry <zxwtry@qq.com>
2018-06-04 20:07:31 +0800

    add L029

--- leetcode/L029_divide_two_integers.c
+++ leetcode/L029_divide_two_integers.c
@@ -4,17 +4,29 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 
 long long L029_divide_long(long long d, long long r) {
     if (d < r) {
         return 0;
     }
-
+    long long times = 1;
+    long long val = r;
+    while (val + val <= d) {
+        times += times;
+        val += val;
+    }
+    return times + L029_divide_long(d - val, r);
 }
 
+
+// Ac 20ms 53.33 %
 int L029_divide(int dividend, int divisor) {
-    if (divisor == 1 || divisor == -1) {
-        return divisor == 1 ? dividend : - dividend;
+    if (divisor == 1) {
+        return dividend;
+    }
+    if (divisor == -1) {
+        return dividend == INT_MIN ? 0 : - dividend;
     }
     if (divisor == 0) {
         return 0;
@@ -24,11 +36,16 @@ int L029_divide(int dividend, int divisor) {
     d = d < 0 ? -d : d;
     r = r < 0 ? -r : r;
     long long ans = L029_divide_long(d, r);
-    if ( (dividend < 0 && divisor > 0) || (dividend < 0 && divisor > 0) ) {
+    if ( (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ) {
         ans = - ans;
     }
-    if (ans < INT_MIN || ans > INT_MAX) {
-        return 0;
-    }
     return (int) ans;
+}
+
+
+void L029() {
+    int d = INT_MIN;
+    int r = -1;
+    printf("a is %d\n", L029_divide(d, r));
+    //printf("a is %d\n", (d / r));
 }
